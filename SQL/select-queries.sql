19720a969ed90d77560b867789cfb937e62ab83d
Subhojit-malik <subhojitmalik151@gmail.com>
2026-08-22 14:02:34 +0530

    Added SQL SELECT examples

---
+++ SQL/select-queries.sql
@@ -0,0 +1,16 @@
+-- Select all columns
+
+SELECT *
+FROM Employees;
+
+-- Select specific column
+
+SELECT Name
+FROM Employees;
+
+-- Select multiple columns
+
+SELECT EmployeeID,
+       Name,
+       Salary
+FROM Employees;
